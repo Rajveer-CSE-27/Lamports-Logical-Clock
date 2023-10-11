45909b9488e9d1b305618030e7ea4dd93d6865f9
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2023-10-11 21:27:30 +0530

    Update lamport_clock.cpp

--- lamport_clock.cpp
+++ lamport_clock.cpp
@@ -3,64 +3,79 @@ using namespace std;
 
 string s;
 
-// Function to simulate Lamport's distributed mutual exclusion algorithm
-void lamport(multimap<int,string> &order, vector<vector<string>> &sentMessages, 
+void lamport(multimap<int,string> &totalOrder, vector<vector<string>> &sentMessages, 
                 map<string,vector<string>> &instructions, map<string,string> &deadlock,
                 vector<string> &processes, int &clock, string curProcess, bool &lock){
     
     int n = processes.size();
-    int index; // Index to track the current instruction for the process
+    int index; // to switch between processes
     
     while(true){
         
-        // Get the index of the current instruction for the process
+        //index of instructions of process
         index = stoi(instructions[curProcess][0]); 
-        if(index >= instructions[curProcess].size())
+        if(index >= instructions[curProcess].size()){
             break;
-        // Get the instruction at the current index
+        }
+        int blocked = 0;
+        for(auto k:processes){
+            if(deadlock[k] == "blocked")
+                blocked++;
+        }
+        if(blocked == n)
+        {
+            lock = true;
+            break;
+        }
+
+        // instruction at 'index'
         s = instructions[curProcess][index];
         index++;
 
-        // Use a stringstream to process the instruction string
+        //stringstream to process the strings
         istringstream ss(s);
         string command;
         ss >> command;
         
         if(command == "send"){
             clock++;
-            string receiver, msg;
-            ss >> receiver >> msg;
-            // Store the sent message and update the event order
-            sentMessages.push_back({to_string(clock),curProcess,msg,receiver});
-            order.insert(make_pair(clock, "sent "+curProcess+" "+msg+" "+receiver));
+            string list, msg;
+            ss >> list >> msg;
+            string temp = list.substr(1);
+            temp.pop_back();
+            istringstream iss(temp);
+            while (iss.good()) {
+                string receiver;
+                getline(iss, receiver, ',');
+                sentMessages.push_back({to_string(clock),curProcess,msg,receiver});
+            }
+            totalOrder.insert(make_pair(clock, "sent "+curProcess+" "+msg+" "+list));
         }
         else if(command == "recv"){
             bool flag = false;
             string sender, msg;
             ss >> sender >> msg;
             if(sentMessages.empty()){
-                // Handle deadlock scenario
                 deadlock[curProcess] = "blocked";
                 instructions[curProcess][0] = to_string(index - 1);
                 instructions[curProcess][1] = to_string(clock);
                 curProcess = sender;
                 index = stoi(instructions[curProcess][0]);
                 clock = stoi(instructions[curProcess][1]);
+                
                 continue;
             }
             for(int i=0;i<sentMessages.size();i++){
                 if(sentMessages[i][1] == sender && sentMessages[i][2] == msg){
-                    // Process received message and update the event order
                     deadlock[curProcess] = "free";
                     clock = max(stoi(sentMessages[i][0]), clock) + 1;
-                    order.insert(make_pair(clock, "received "+curProcess+" "+msg+" "+sender));
+                    totalOrder.insert(make_pair(clock, "received "+curProcess+" "+msg+" "+sender));
                     flag = true;
                     sentMessages.erase(sentMessages.begin()+i);
                     break;
                 }
             }
             if(!flag){
-                // Handle deadlock scenario
                 deadlock[curProcess] = "blocked";
                 instructions[curProcess][0] = to_string(index-1);
                 instructions[curProcess][1] = to_string(clock);
@@ -75,22 +90,12 @@ void lamport(multimap<int,string> &order, vector<vector<string>> &sentMessages,
             string msg;
             ss >> msg;
             clock++;
-            // Update the event order for printing
-            order.insert(make_pair(clock,"printed "+curProcess+" "+msg));
+            totalOrder.insert(make_pair(clock,"printed "+curProcess+" "+msg));
         }
         else if(command == "end"){
             clock = 0;
         }
-        int blocked = 0;
-        for(auto k:processes){
-            if(deadlock[k] == "blocked")
-                blocked++;
-        }
-        if(blocked == n)
-        {
-            lock = true;
-            break;
-        }
+        
         if(deadlock[curProcess] != "blocked")
             instructions[curProcess][0] = to_string(index);
         
@@ -115,29 +120,37 @@ void lamport(multimap<int,string> &order, vector<vector<string>> &sentMessages,
     }
 }
 
+
 int main(){
 
     int clock = 0;
     string curProcess = "";
     string process;
     bool lock = false;
-    int n; // Number of processes;
+    int n; // no of processes;
 
-    multimap<int,string> order; // Total order of all events
-    vector<vector<string>> sentMessages;// List of messages sent (in transit)
-    map<string,vector<string>> instructions; // User input
+    multimap<int,string> totalOrder; //total order of all events
+    vector<vector<string>> sentMessages;// list of messages sent (transit)
+    map<string,vector<string>> instructions; //user input
     map<string,string> deadlock;
     
+    set<pair<string,string>> send;
+    set<pair<string,string>> recv;
+
     cout << "Enter the number of processes: "; 
     cin >> n;
     getline(cin,s);
     vector<string> processes(n);
     int ind = 0;
+    bool proceed = true;
 
+    cout << " enter the operations: \n";
     while(s != "exit"){
         getline(cin,s);
         if(s == "exit")
             break;
+        if(s == "")
+            continue;
         istringstream ss(s);
         string command;
         ss >> command;
@@ -154,40 +167,74 @@ int main(){
             instructions[process].push_back("0");
             continue;
         }
+        else if(command == "send"){
+            send.insert(make_pair(process,s));
+        }
+        else if(command == "recv"){
+            recv.insert(make_pair(process,s));
+        }
+        else if(command == "print" || command == "end"){
+            // do nothing
+            if(command == "end"){
+                ss >> command;
+                if(command != "process")
+                {
+                    cout << "\nPlease check your input and try again. Follow the input format.\n";
+                    return 0;
+                }
+            }
+        }
+        else{
+            cout << "\nThere is no operation, named " << command << ". Please check your input and try again.\n";
+            proceed = false;
+            break;
+        }
         instructions[process].push_back(s);
     }
 
     if(instructions.size() != n){
-        cout << n << " processes are not there in the input. Please check your input !!" << endl;
+        cout << "Operations for "<< n << " processes were not given in the input. Please check yout input !!" << endl;
         return 0;
     }
-    
-    lamport(order,sentMessages,instructions,deadlock,processes,clock,curProcess,lock);
-    
-    cout << "Before : \n";
-    for(auto i : processes){
-        cout << i << " -> " << deadlock[i] << endl;
-    }
 
-    if(lock)
-        cout << "Since all the processes are waiting for the resources and no messages are in transit. Hence Deadlocked!!"<<endl;
-    else{
-        for(auto i : processes){
-            while(deadlock[i] == "blocked"){
+
+    if(proceed){
+    
+        lamport(totalOrder,sentMessages,instructions,deadlock,processes,clock,curProcess,lock);
+
+        if(lock)
+            cout << "Since all the processes are waiting for the resources and no messages are in transit. Hence Deadlocked!!"<<endl;
+        else{
+            bool flag = false;
+            for(auto i : processes){
+                int cntr = 0;
+                while(deadlock[i] == "blocked"){
+                    int clk = stoi(instructions[i][1]);
+                    lamport(totalOrder,sentMessages,instructions,deadlock,processes,clk,i,lock);
+                    cntr++;
+                    if(cntr >= n){
+                        lock = true;
+                        break;
+                    }
+                }
+            }
+            for(auto i : processes){
                 int clk = stoi(instructions[i][1]);
-                lamport(order,sentMessages,instructions,deadlock,processes,clk,i,lock);
+                lamport(totalOrder,sentMessages,instructions,deadlock,processes,clk,i,lock);
             }
         }
-    }
-    for(auto i = order.begin(); i != order.end();i++){
-        cout << i->second << " " << i->first << endl;
-    }
 
-    cout << "Messages in transit : \n";
-    for(auto i =0;i<sentMessages.size();i++){
-        for(auto j : sentMessages[i]){
-            cout << j << " ";
+        cout << "\n\nThis is the total order(so far): \n";
+        if(totalOrder.empty())
+            cout << "empty\n\n";
+        else{
+            for(auto i = totalOrder.begin(); i != totalOrder.end();i++){
+                cout << i->second << " " << i->first << endl;
+            }
         }
-        cout << endl;
+        if(lock){
+            cout << "\n\nSystem Deadlocked\n\n" << endl;
+        }
+        else cout << "\n\nNo deadlock\n\n" << endl;
     }
 }
